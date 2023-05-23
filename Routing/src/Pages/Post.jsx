ceff9e4d3a16038c7f7a0b92b8f0682005493cdf
Ramiz Alhaddad <ramizalhaddad@Ramizs-MacBook-Air.local>
2023-05-22 19:24:46 -0700

    add comments into specific post

--- Routing/src/Pages/Post.jsx
+++ Routing/src/Pages/Post.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { useLoaderData } from "react-router-dom";
 
 export function Post() {
-  const post = useLoaderData();
-
-  if (!post) {
-    return <div>Loading...</div>;
-  }
+  const { title, body, comments } = useLoaderData();
 
   return (
     <div className='container'>
-      <h1 className='page-title'>{post.title}</h1>
-      <span className='page-subtitle'>By: {post.author}</span>
-      <div>{post.body}</div>
+      <h1 className='page-title'>{title}</h1>
+      <span className='page-subtitle'>By: {}</span>
+      <div>{body}</div>
+      <h3 className='mt-4 mb-2'>Comments</h3>
+      <div className='card-stack'>
+        {comments.map(comment => (
+          <div className='card' key={comment.id}>
+            <div className='card-body'>
+              <div className='text-sm mb-1'>{comment.email}</div>
+              {comment.body}
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
